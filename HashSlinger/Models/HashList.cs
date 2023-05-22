29bb3eb947d8b6ccc7f8658910cf1a99bc1947af
UncleSp1d3r <unclespider@protonmail.com>
2023-05-22 15:59:01 -0400

    Moved to a minimal API setup

--- HashSlinger/Models/HashList.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace HashSlinger.Models
-{
-    public class HashList
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-    }
-}
--- HashSlinger/Program.cs
+++ HashSlinger/Program.cs
@@ -1,15 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using HashSlinger.Data;
-
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<HashSlingerContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("HashSlingerContext") ??
-                      throw new InvalidOperationException("Connection string 'HashSlingerContext' not found.")));
 
 // Add services to the container.
-
-builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -25,8 +16,29 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
 
-app.MapControllers();
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast")
+.WithOpenApi();
 
 app.Run();
+
+internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
