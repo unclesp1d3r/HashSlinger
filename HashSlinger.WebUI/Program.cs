8668d03648b68108042c34cb3b51254044f8ec6e
UncleSp1d3r <unclespider@protonmail.com>
2023-06-22 12:11:44 -0400

    Converted WebUI to WASM

--- HashSlinger.WebUI/Program.cs
+++ HashSlinger.WebUI/Program.cs
@@ -1,35 +1,21 @@
+using HashSlinger.WebUI;
 using HashSlinger.WebUI.Data;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using Refit;
 
-WebApplicationBuilder? builder = WebApplication.CreateBuilder(args);
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
-builder.Services.AddMudServices();
-builder.Services.AddHttpClient();
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddRefitClient<IHashSlingerApi>()
-    .ConfigureHttpClient(c => c.BaseAddress = new Uri("http://localhost:5090"));
+    .ConfigureHttpClient(c =>
+    {
+        c.BaseAddress = new Uri("http://localhost:5090/api/v1");
+        c.DefaultVersionPolicy = HttpVersionPolicy.RequestVersionOrHigher;
+    });
+builder.Services.AddMudServices();
 
-WebApplication? app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
-
-
-app.Run();
+await builder.Build().RunAsync();
